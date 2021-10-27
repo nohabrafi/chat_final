0e7adf83e4c0b01acc847495339dc27f9fa7fb4e
nohabrafi <charliebbarkin@tutanota.com>
2021-10-27 11:04:20 +0200

    First working thing

---
+++ server/public/app/app.js
@@ -0,0 +1,220 @@
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+var iUseTheDB = false;
+
+const socket = io("http://localhost:3000");
+
+/*####### socket.io #######*/
+
+socket.on("connect", () => {
+    console.log(`you with id ${socket.id} connected.`);
+    appendMessageSIO(`you with id ${socket.id} connected.`);
+});
+
+socket.on("disconnect", () => {
+    console.log(`you disconnected.`)
+    appendMessageSIO(`you disconnected.`);
+});
+
+socket.on("receive-message", message => { // aztán figyelünk a válaszra  szervertől. ha ez a válasz megegyezik az eventünkel amit akarunk akkor
+    // csinaljuk amit kell
+    appendMessageSIO(message);
+});
+
+socket.on("user-connected", connectMsg => {
+    appendMessageSIO(connectMsg);
+});
+
+socket.on("user-disconnected", disconnectMsg => {
+    appendMessageSIO(disconnectMsg);
+});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+
+        socket.emit("send-message", input.value); // 1. eloszor emit elünk valamit egy custom event névvel a serverre
+
+        appendMessageSIO(input.value);
+
+        input.value = "";
+    }
+});
+
+
+function appendMessageSIO(message) {
+
+    let theMessage = message + "----" + new Date(Date.now()).toString().substring(0, 24);
+
+    let messageHolder = document.getElementById("message-holder");
+    let item = document.createElement('li');
+    item.innerHTML = theMessage;
+    messages.appendChild(item);
+    messageHolder.scrollTop = messageHolder.scrollHeight;
+
+}
+
+
+document.addEventListener('keydown', e => {
+    if (e.target.matches('input')) return;
+
+    if (e.key === 'c') socket.connect();
+    if (e.key === 'd') socket.disconnect();
+});
+
+
+
+
+
+/*####### socket.io #######*/
+
+
+
+// const usernameStyle = (username) => {
+//     return "<span id='username-style'>" + username + ":" + "</span>";
+// }
+
+// const messageStyle = (message) => {
+//     return "<span id='message-style'>" + message + "</span>";
+// }
+
+// const sentAtStyle = (sentAt) => {
+//     return "<span id='sentAt-style'>" + sentAt + "</span>";
+// }
+
+// const üzenetÖsszefűzőFüggvény = (username, message, sentAt) => {
+//     return `<span id='username-style'>${username}:</span><span id='message-style'>${message}</span><span id='sentAt-style'>${sentAt}</span>`;
+// }
+
+// if (sessionStorage.getItem("username")) {
+
+//     window.onload = () => {
+//         user = sessionStorage.getItem("username");
+//         document.getElementById('curr-user').innerHTML = "logged in: " + sessionStorage.getItem("username");
+//         console.log("user" + user + " entered the chat");
+//         document.title = "Chat of " + user;
+//         getAllMessages();
+
+//     }
+
+// } else {
+//     window.location.href = "http://localhost:8000/login_page";
+// }
+
+
+
+// function appendMessageDB(username, message, sentAt) {
+
+//     let messageHolder = document.getElementById("message-holder");
+//     let item = document.createElement('li');
+//     // if (username == user) {
+//     //     item.style.textAlign = 'right';
+//     // }
+//     item.innerHTML = üzenetÖsszefűzőFüggvény(username, message, sentAt);
+//     messages.appendChild(item);
+//     messageHolder.scrollTop = messageHolder.scrollHeight;
+//     //window.scrollTo(0, document.body.scrollHeight);
+// }
+
+// const logOutUser = () => {
+//     sessionStorage.clear();
+//     console.log(sessionStorage);
+//     console.log("session storage cleared, user logged out");
+//     window.location.href = "http://localhost:8000/login_page";
+// }
+
+
+
+// const myPostMessage = (user, message) => {
+
+//     fetch('http://localhost:8000/post_message', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify({
+//                 username: user,
+//                 message: message
+//             })
+//         }).then(res => {
+//             return res.json();
+//         }).then(data => {
+//             getOneMessage();
+//             console.log(data);
+//         })
+//         .catch(error => console.log(error));
+// }
+
+// const getAllMessages = () => { /// leegyszerusiteni !!!
+
+//     fetch('http://localhost:8000/get_messages')
+//         .then(res => res.json())
+//         .then(data => {
+
+
+//             for (var i = 0; i < data.length; i++) {
+
+//                 let username = data[i].username;
+//                 let message = data[i].message;
+//                 let sentAt = new Date(data[i].date).toString().substring(0, 24);
+
+//                 let messageHolder = document.getElementById("message-holder");
+//                 let item = document.createElement('li');
+//                 if (username == user) {
+//                     item.style.textAlign = 'right';
+//                 }
+//                 item.innerHTML = usernameStyle(username) + " " + messageStyle(message) + " " + sentAtStyle(sentAt);
+//                 messages.appendChild(item);
+//                 messageHolder.scrollTop = messageHolder.scrollHeight;
+//                 //window.scrollTo(0, document.body.scrollHeight);
+//             }
+//         })
+//         .catch(error => console.log(error));
+
+//     console.log("got all messages")
+// }
+
+// const getOneMessage = () => { // itt is visszajön az összes üzenet, nem ideális
+
+//     fetch('http://localhost:8000/get_messages')
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log("message came back");
+
+//             let username = data[data.length - 1].username;
+//             let message = data[data.length - 1].message;
+//             let sentAt = new Date(data[data.length - 1].date).toString().substring(0, 24);
+
+//             console.log("username: " + username + ";" + " " + "message: " + message + " " + "sentAt: " + sentAt + ";");
+
+//             let messageHolder = document.getElementById("message-holder");
+//             var item = document.createElement('li');
+//             if (username == user) {
+//                 item.style.textAlign = 'right';
+//             }
+//             item.innerHTML = usernameStyle(username) + " " + messageStyle(message) + " " + sentAtStyle(sentAt);
+//             messages.appendChild(item);
+//             messageHolder.scrollTop = messageHolder.scrollHeight;
+
+//         })
+//         .catch(error => console.log(error));
+
+// }
+
+// const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }; // opciók a showtime fügvényhez
+
+// function showTime(givenTime) {
+//     var timeNow = new Date(givenTime);
+//     var hours = timeNow.getHours();
+//     var minutes = timeNow.getMinutes();
+//     var seconds = timeNow.getSeconds();
+//     //var timeString = "" + ((hours > 12) ? hours - 12 : hours);
+//     var timeString = "" + hours;
+//     timeString += ((minutes < 10) ? ":0" : ":") + minutes;
+//     timeString += ((seconds < 10) ? ":0" : ":") + seconds;
+//     //timeString += (hours >= 12) ? " P.M." : " A.M.";
+//     //document.htmlClock.timeField.value = timeString;
+//     //timerID = setTimeout("showTime()", 1000);
+//     return timeString;
+// }
---
+++ server/server.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public')); // serves static files from the 'public' folder
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // allow requests from anywhere ('*')
+
+
+
+// const uri = process.env.DB_CONNECTON_good;
+// const options = { useNewUrlParser: true };
+// const callback = () => {
+//     console.log('connected to DB!')
+// };
+// mongoose.connect(uri, options, callback);
+// //############ CONNECTION TO DB ############//
+
+server.listen(process.env.PORT || 3000, () => console.log(`Server listening on port ${process.env.PORT}`));
+
+app.get('/', (req, res) => {
+    res.render("app");
+});
+
+io.on("connection", (socket) => {
+    // the message to display in a variable because it is used more than once
+    let connectMsg = `${socket.id} connected`;
+    // just console log it on the server
+    console.log(connectMsg);
+    // upon the CONNECTION of a socket the server broadcasts a message to the other connected sockets to let them know
+    socket.broadcast.emit("user-connected", connectMsg);
+
+    socket.on("disconnect", (reason) => {
+        // the message to display in a variable because it is used more than once
+        let disconnectMsg = `${socket.id} disconnected due to "${reason}"`;
+        // just console log it on the server
+        console.log(disconnectMsg);
+        // upon the DISCONNECTION of a socket the server broadcasts a message to the other connected sockets to let them know
+        socket.broadcast.emit("user-disconnected", disconnectMsg);
+    });
+
+    socket.on("send-message", message => { // 2. aztan az eventünk a custom event névvel bejön a serverre és eldöntjuk hog ymi legyen vele
+        socket.broadcast.emit("receive-message", message); // az lesz vele hogy mivel ez egy egy chat üzenet kiküldjuk akinek kell (itt mindenkinek)
+        // itt is egy custom event nevet használunk. a broadcast flaggel kikuldi mindenkinek kivéve a feladónak
+    })
+});
+
+Mára: socket.io authentication valahogy
